035f2fde146e46453f6ada0fa0805bf7790826e2
Michael Murray <cloud19912003@live.com>
2020-09-20 20:08:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentance)
-  
+  sentence.split.map {|word| word.reverse}.join(" ")
   sentance_array = sentance.split(" ")
   sentance_array.each.reverse_each
   sentance_array.join(" ")
